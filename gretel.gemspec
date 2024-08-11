c5c84119283896299a7fff1a4efbc37876956cda
Alexey Poimtsev <25312+alec-c4@users.noreply.github.com>
2024-08-11 12:14:15 +0300

    Unlock rails 7.2+ (#45)

--- gretel.gemspec
+++ gretel.gemspec
@@ -21,7 +21,7 @@ Gem::Specification.new do |gem|
 
   gem.add_dependency "railties", ">= 6.1"
   gem.add_dependency "actionview", ">= 6.1"
-  gem.add_development_dependency "sqlite3", "< 2"
+  gem.add_development_dependency "sqlite3", '~> 1.4'
   gem.add_development_dependency "rspec-rails"
   gem.add_development_dependency "simplecov"
   gem.add_development_dependency "simplecov-erb"
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -2,27 +2,25 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2013_01_22_163051) do
-
+ActiveRecord::Schema[7.2].define(version: 2013_01_22_163051) do
   create_table "issues", force: :cascade do |t|
     t.string "title"
     t.integer "project_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", precision: nil
+    t.datetime "updated_at", precision: nil
   end
-
 end
