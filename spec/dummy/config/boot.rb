786b6750aad4c1b9dafce0b16ac7356a8eada761
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2026-01-31 13:32:27 +0900

    fix Gemfile reference from dummy project

--- spec/dummy/config/boot.rb
+++ spec/dummy/config/boot.rb
@@ -1,10 +1,13 @@
 require 'rubygems'
-gemfile = File.expand_path('../../../../Gemfile', __FILE__)
 
-if File.exist?(gemfile)
-  ENV['BUNDLE_GEMFILE'] = gemfile
-  require 'bundler'
-  Bundler.setup
+unless ENV['BUNDLE_GEMFILE']
+  gemfile = File.expand_path('../../../../Gemfile', __FILE__)
+  if File.exist?(gemfile)
+    ENV['BUNDLE_GEMFILE'] = gemfile
+  end
 end
+
+require 'bundler'
+Bundler.setup
 
 $:.unshift File.expand_path('../../../../lib', __FILE__)
