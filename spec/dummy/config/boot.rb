be3585c451e11da6a6a5d92c03b8fd88c5a03e23
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2026-01-31 13:35:32 +0900

    add nocov

--- spec/dummy/config/boot.rb
+++ spec/dummy/config/boot.rb
@@ -1,11 +1,13 @@
 require 'rubygems'
 
+# :nocov:
 unless ENV['BUNDLE_GEMFILE']
   gemfile = File.expand_path('../../../../Gemfile', __FILE__)
   if File.exist?(gemfile)
     ENV['BUNDLE_GEMFILE'] = gemfile
   end
 end
+# :nocov:
 
 require 'bundler'
 Bundler.setup
